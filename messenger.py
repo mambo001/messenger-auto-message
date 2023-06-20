3d4e4064d8932c2022bf106326dae24d1f11640b
Reuben <reuben.gorda@gmail.com>
2023-06-20 14:58:52 +0800

    replace browser being used. add chrome options

--- messenger.py
+++ messenger.py
@@ -7,7 +7,15 @@ import os
 import time
 
 
-driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--window-size=1920,1080")
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.implicitly_wait(10)
 # driver = uc.Chrome()
 #going to messenger website
 driver.get("https://www.messenger.com")
